e8ebf0afb88f9542c6336c157b3089bc944739bb
Pascal J. Schroeder <65792599+PascalJSchroeder@users.noreply.github.com>
2025-04-23 16:46:58 +0200

    Update RAD_POW_ANA.py

--- RAD_POW_ANA.py
+++ RAD_POW_ANA.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created by Pascal J. Schroeder at INSP
+"""
+
 import numpy as np
 import os
 from utils import create_directory, solve_integral, save_plot_data
